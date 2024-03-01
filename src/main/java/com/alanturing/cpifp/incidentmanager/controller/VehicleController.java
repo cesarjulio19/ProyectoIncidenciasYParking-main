aeb189a059e62c44e288e242d640a892f3ac5b5c
Boorja21 <115183112+boorjabraavo21@users.noreply.github.com>
2024-03-01 18:36:41 +0100

    cambios

--- src/main/java/com/alanturing/cpifp/incidentmanager/controller/VehicleController.java
+++ src/main/java/com/alanturing/cpifp/incidentmanager/controller/VehicleController.java
@@ -67,7 +67,7 @@ public class VehicleController {
     }
 
     @DeleteMapping("/{id}")
-    private @ResponseBody String deleteVehicle(@PathVariable int id) {
+    private @ResponseBody String deleteVehicle(@PathVariable("idV") int id) {
         service.deleteVehicle(id);
         return "Deleted";
     }
